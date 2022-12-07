5f683700926a9c2396bf75254ac116b48cf5d678
vayunandhan <vyekkaluri@umass.edu>
2022-12-07 18:07:42 -0500

    added Companies view

---
+++ src/views/Companies.vue
@@ -0,0 +1,32 @@
+<template>
+  <main id="companies">
+    <h1>Here, You can see the aggregated results for each company</h1>
+    <FilterForCompany @filtered-on-company="filterOnCompany"></FilterForCompany>
+    <CompanyWiseData :filter-key="searchObject"></CompanyWiseData>
+  </main>
+</template>
+
+
+<script>
+import FilterForCompany from "../components/FilterForCompany.vue";
+import CompanyWiseData from "../components/CompanyWiseData.vue";
+
+export default {
+  name: "Companies",
+  setup() {},
+  components: {
+    FilterForCompany,
+    CompanyWiseData,
+  },
+  data() {
+    return {
+      searchObject: {},
+    };
+  },
+  methods: {
+    filterOnCompany(searchCriteria) {
+      this.searchObject = searchCriteria;
+    },
+  },
+};
+</script>
