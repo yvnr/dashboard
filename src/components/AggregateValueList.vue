93fa5ad299f191711c63fdaec5712ef2e4a07472
vayunandhan <vyekkaluri@umass.edu>
2022-12-07 17:53:38 -0500

    added AggregateValueList component

---
+++ src/components/AggregateValueList.vue
@@ -0,0 +1,44 @@
+<template>
+  <div style="padding-top: 10px">
+    <CContainer fluid>
+      <CRow>
+        <CCol
+          :key="aggregateValue.id"
+          v-for="aggregateValue in aggregateValues"
+        >
+          <AggregateValue :aggregateValue="aggregateValue"></AggregateValue>
+        </CCol>
+      </CRow>
+    </CContainer>
+  </div>
+</template>
+
+<script>
+import AggregateValue from "./AggregateValue.vue";
+import { CContainer, CRow, CCol, CWidgetStatsF } from "@coreui/vue";
+import { CIcon } from "@coreui/icons-vue";
+import * as icon from "@coreui/icons";
+
+export default {
+  name: "AggregateValueList",
+  props: {
+    aggregateValues: Array,
+  },
+  components: {
+    AggregateValue,
+    CIcon,
+    CContainer,
+    CRow,
+    CCol,
+    CWidgetStatsF,
+  },
+  setup() {
+    return {
+      icon,
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
