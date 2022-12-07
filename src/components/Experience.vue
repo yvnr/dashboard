f3e8123d83fa8367695744e2849aaaac9c15f373
vayunandhan <vyekkaluri@umass.edu>
2022-12-07 17:59:07 -0500

    added Experience component

---
+++ src/components/Experience.vue
@@ -0,0 +1,55 @@
+<template>
+  <CAccordionItem :item-key="experience.id">
+    <CAccordionHeader>
+      <CContainer>
+        <CRow>
+          <CCol xs="6" s="8" md="8" class="first"
+            ><b>Role: </b>{{ experience.role }}
+          </CCol>
+          <CCol class="middle"> <b>Status: </b>{{ experience.result }} </CCol>
+          <CCol class="last"><b>Posted On: </b>{{ experience.on }}</CCol>
+        </CRow>
+      </CContainer>
+    </CAccordionHeader>
+    <CAccordionBody>
+      <div v-text="experience.data" style="white-space: pre-wrap"></div>
+    </CAccordionBody>
+  </CAccordionItem>
+</template>
+
+
+<script>
+import {
+  CAccordionItem,
+  CAccordionHeader,
+  CAccordionBody,
+  CContainer,
+  CRow,
+  CCol,
+} from "@coreui/vue";
+
+export default {
+  name: "Experience",
+  setup() {},
+  props: {
+    experience: Object,
+  },
+  components: {
+    CAccordionItem,
+    CAccordionHeader,
+    CAccordionBody,
+    CContainer,
+    CRow,
+    CCol,
+  },
+};
+</script>
+
+<style scoped>
+CAccordionItem {
+  font-size: 2rem;
+}
+.first {
+  left: 0rem;
+}
+</style>
