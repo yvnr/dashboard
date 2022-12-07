39e210a13ac5edb7d6785d2dcb9084f32d39d0cb
vayunandhan <vyekkaluri@umass.edu>
2022-12-07 17:54:38 -0500

    added ToggleAddJobApplicationForm component

---
+++ src/components/ToggleAddJobApplicationForm.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container-fluid d-flex p-2 justify-content-center">
+    <button @click="onClick()">ADD JOB APPLICATION</button>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {},
+  methods: {
+    onClick() {
+      this.$emit("toggle-add-job-application-form");
+    },
+  },
+};
+</script>
+
+<style scoped>
+button {
+  align-items: center;
+  justify-content: space-between;
+  padding: 3px 7px;
+}
+</style>
