ce676254ea00de47fde6712ff59202431ce19825
vayunandhan <vyekkaluri@umass.edu>
2022-12-07 17:59:40 -0500

    added ExperiencesList component

---
+++ src/components/ExperiencesList.vue
@@ -0,0 +1,37 @@
+<template>
+  <div container-fluid>
+    <CAccordion always-open>
+      <div :key="experience.id" v-for="experience in experiences">
+        <Experience :experience="experience"></Experience>
+      </div>
+    </CAccordion>
+  </div>
+</template>
+
+<script>
+import Experience from "./Experience.vue";
+import {
+  CAccordion,
+  CAccordionItem,
+  CAccordionHeader,
+  CAccordionBody,
+} from "@coreui/vue";
+
+export default {
+  name: "ExperiencesList",
+  setup() {},
+  props: {
+    experiences: Array,
+  },
+  components: {
+    Experience,
+    CAccordion,
+    CAccordionItem,
+    CAccordionHeader,
+    CAccordionBody,
+  },
+};
+</script>
+
+<style scoped>
+</style>
