5a8b5ebd0d9d10a318cd19d30334d7daf2bb8969
vayunandhan <vyekkaluri@umass.edu>
2022-12-07 17:55:39 -0500

    added AddJobApplication component

---
+++ src/components/AddJobApplication.vue
@@ -0,0 +1,138 @@
+<template lang="">
+ <div class="container-fluid">
+     <form @submit="onSubmit" class="add-form">
+    <div class="form-control">
+      <label>Company</label>
+      <input type="text" v-model="company" name="company" placeholder="Add Company" />
+    </div>
+    <div class="form-control">
+      <label>Position</label>
+      <input type="text" v-model="position" name="position" placeholder="Add Position"/>
+    </div>
+    <div class="form-control">
+      <label>Duration</label>
+      <input type="text" v-model="duration" name="duration" placeholder="Add Duration"/>
+    </div>
+    <div class="form-control">
+      <label>Job ID</label>
+      <input type="text" v-model="jobID" name="jobID" placeholder="Add Job ID"/>
+    </div>
+    <div class="form-control">
+      <label>Location</label>
+      <input type="text" v-model="location" name="location" placeholder="Add Location"/>
+    </div>
+    <div class="form-control">
+      <label>Status</label>
+      <input type="text" v-model="status" name="status" placeholder="Add Status"/>
+    </div>
+    <input type="submit" value="Add Application" class="submit" />
+  </form>
+ </div>
+</template>
+
+<script>
+export default {
+  name: "AddJobApplication",
+  setup() {},
+  data() {
+    return {
+      company: "",
+      position: "",
+      duration: "",
+      jobID: "",
+      location: "",
+      status: "Applied",
+    };
+  },
+  methods: {
+    onSubmit(e) {
+      e.preventDefault();
+
+      if (this.validationForEmptyValues()) {
+        return;
+      }
+
+      const application = {
+        company: this.company,
+        position: this.position,
+        duration: this.duration,
+        jobID: this.jobID,
+        location: this.location,
+        status: this.status,
+      };
+      console.log(application);
+      // TODO: need to post this data
+
+      this.company = "";
+      this.position = "";
+      this.duration = "";
+      this.jobID = "";
+      this.location = "";
+      this.status = "Applied";
+    },
+    validationForEmptyValues() {
+      if (!this.company) {
+        alert("Please enter the company you applied for");
+        return true;
+      }
+      if (!this.position) {
+        alert("Please enter position you applied for");
+        return true;
+      }
+      if (!this.duration) {
+        alert("Please enter duration you applied for");
+        return true;
+      }
+      if (!this.jobID) {
+        alert("Please enter Job ID");
+        return true;
+      }
+      if (!this.location) {
+        alert("Please enter the location");
+        return true;
+      }
+      if (!this.status) {
+        alert("Please enter your application's current status");
+        return true;
+      }
+      return false;
+    },
+  },
+};
+</script>
+
+
+<style scoped >
+.submit {
+  padding: 3px 7px;
+}
+
+.add-form {
+  margin-bottom: 40px;
+}
+.form-control {
+  margin: 20px 0;
+}
+.form-control label {
+  display: block;
+}
+.form-control input {
+  width: 100%;
+  height: 40px;
+  margin: 5px;
+  padding: 3px 7px;
+  font-size: 17px;
+}
+.form-control-check {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.form-control-check label {
+  flex: 1;
+}
+.form-control-check input {
+  flex: 2;
+  height: 20px;
+}
+</style>
