dd10cdad1778d87ffbe2c06187aab8e833516598
vayunandhan <vyekkaluri@umass.edu>
2022-12-07 18:06:03 -0500

    added routes for applications, companies and interviewExperiences views

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,25 +1,22 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
-
-const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView,
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
-  },
-];
+import { createRouter, createWebHistory } from 'vue-router'
+import Applications from '../views/Applications.vue'
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
-});
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      component: Applications
+    },
+    {
+      path: '/companies',
+      component: () => import('../views/Companies.vue')
+    },
+    {
+      path: '/interviewExperiences',
+      component: () => import('../views/InterviewExperiences.vue')
+    },
+  ],
+})
 
-export default router;
+export default router
