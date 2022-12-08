63f375c368d044eb85bddf938b8fd651b4d25b01
vayunandhan <vyekkaluri@umass.edu>
2022-12-08 01:33:46 -0500

    Added HeaderComponent component

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,51 @@
+<template>
+    <CHeader>
+  <CContainer fluid>
+    <CHeaderBrand href="#">Header</CHeaderBrand>
+    <CHeaderToggler @click="visible = !visible" />
+    <CCollapse class="header-collapse" :visible="visible">
+      <CHeaderNav>
+        <CNavItem>
+          <CNavLink href="#" active>
+            Home
+          </CNavLink>
+        </CNavItem>
+        <CNavItem>
+          <CNavLink href="#">Link</CNavLink>
+        </CNavItem>
+        <CDropdown togglerText="Dropdown" variant="nav-item">
+          <CDropdownItem href="#">Action</CDropdownItem>
+          <CDropdownItem href="#">Another action</CDropdownItem>
+          <CDropdownItem href="#">Something else here</CDropdownItem>
+        </CDropdown>
+        <CNavItem>
+          <CNavLink href="#" disabled>
+            Disabled
+          </CNavLink>
+        </CNavItem>
+      </CHeaderNav>
+      <!-- <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form> -->
+    </CCollapse>
+  </CContainer>
+</CHeader>
+</template>
+
+<script>
+import {
+  CHeader, CContainer, CHeaderBrand, CHeaderToggler, CCollapse, CHeaderNav, CNavItem, CNavLink, CDropdown, CDropdownItem,
+} from '@coreui/vue';
+
+export default {
+  data() {
+    return {
+      visible: true,
+    };
+  },
+  components: {
+    CHeader, CContainer, CHeaderBrand, CHeaderToggler, CCollapse, CHeaderNav, CNavItem, CNavLink, CDropdown, CDropdownItem,
+  },
+};
+</script>
