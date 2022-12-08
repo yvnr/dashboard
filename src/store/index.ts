0d168b4a749d64af6956bddd9492d974eed2e20e
Phanindra Pvs <vpamidimukka@umass.edu>
2022-12-08 05:29:25 -0500

    added firebase authentication and implemented vue store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,27 @@
-import { createStore } from 'vuex';
+import { createStore, StoreOptions } from 'vuex';
+import { UserCredential } from 'firebase/auth';
 
-export default createStore({
+const store: StoreOptions<UserState> = {
   state: {
-  },
-  getters: {
-  },
-  mutations: {
+    name: undefined,
+    email: undefined,
+    univName: undefined,
+    uid: undefined,
+    univId: undefined,
+    sessionToken: undefined,
   },
   actions: {
+    async populateUser(context, payload: UserCredential) {
+      const { state } = context;
+      const { user } = payload;
+      state.email = user.email as string;
+      state.name = user.displayName as string;
+      state.uid = user.uid;
+      const { token, claims } = await user.getIdTokenResult();
+      state.univId = claims.univId;
+      state.sessionToken = token;
+    },
   },
-  modules: {
-  },
-});
+};
+
+export default createStore(store);
