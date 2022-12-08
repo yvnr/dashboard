ce1bce680af127793f1b55b8076ed9a32ad33b7d
vayunandhan <vyekkaluri@umass.edu>
2022-12-08 01:34:04 -0500

    Added FooterComponent component

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <CFooter position="fixed">
+    <div>
+      <span><b>CS520</b></span>
+    </div>
+    <div>
+      <span><u><i>The Makers</i></u></span>
+    </div>
+  </CFooter>
+</template>
+
+<script>
+import { CFooter } from '@coreui/vue';
+
+export default {
+  components: {
+    CFooter,
+  },
+
+};
+</script>
