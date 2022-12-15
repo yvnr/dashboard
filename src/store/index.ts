8cf155627d03e5ccdb8887a5c3f327082ef0a84c
Phanindra Pvs <vpamidimukka@umass.edu>
2022-12-15 04:41:23 -0500

    added sessionstorage implementation for the persisting user data

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,25 +3,54 @@ import { UserCredential } from 'firebase/auth';
 
 const store: StoreOptions<UserState> = {
   state: {
-    name: undefined,
-    email: undefined,
-    univName: undefined,
-    uid: undefined,
-    univId: undefined,
-    sessionToken: undefined,
+    name: sessionStorage.getItem('name'),
+    email: sessionStorage.getItem('email'),
+    univName: sessionStorage.getItem('univName'),
+    uid: sessionStorage.getItem('uid'),
+    univId: sessionStorage.getItem('univId'),
+    sessionToken: sessionStorage.getItem('sessionToken'),
   },
   actions: {
-    async populateUser(context, payload: UserCredential) {
-      const { state } = context;
+    async createSession(context, payload: UserCredential) {
       const { user } = payload;
-      state.email = user.email as string;
-      state.name = user.displayName as string;
-      state.uid = user.uid;
+      if (user.email) {
+        sessionStorage.setItem('email', user.email);
+      }
+      if (user.displayName) {
+        sessionStorage.setItem('name', user.displayName);
+      }
+      if (user.uid) {
+        sessionStorage.setItem('uid', user.uid);
+      }
       const { token, claims } = await user.getIdTokenResult();
-      state.univId = claims.univId;
-      state.sessionToken = token;
+      sessionStorage.setItem('sessionToken', token);
+      sessionStorage.setItem('univId', claims.univId);
+      context.commit('updateStore');
+    },
+    async deleteSession(context) {
+      sessionStorage.clear();
+      context.commit('clearStore');
     },
   },
+  mutations: {
+    updateStore: (state) => {
+      state.name = sessionStorage.getItem('name');
+      state.email = sessionStorage.getItem('email');
+      state.univName = sessionStorage.getItem('univName');
+      state.uid = sessionStorage.getItem('uid');
+      state.univId = sessionStorage.getItem('univId');
+      state.sessionToken = sessionStorage.getItem('sessionToken');
+    },
+    clearStore: (state) => {
+      state.name = undefined;
+      state.email = undefined;
+      state.univName = undefined;
+      state.uid = undefined;
+      state.univId = undefined;
+      state.sessionToken = undefined;
+    },
+  },
+
 };
 
 export default createStore(store);
