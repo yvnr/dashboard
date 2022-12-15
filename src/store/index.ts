42e626d96ea2869bdc8b562a0d3963a119e254b6
Phanindra Pvs <vpamidimukka@umass.edu>
2022-12-15 05:05:57 -0500

    changed session storage to local storage for user data to even support the browser session close

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,43 +3,43 @@ import { UserCredential } from 'firebase/auth';
 
 const store: StoreOptions<UserState> = {
   state: {
-    name: sessionStorage.getItem('name'),
-    email: sessionStorage.getItem('email'),
-    univName: sessionStorage.getItem('univName'),
-    uid: sessionStorage.getItem('uid'),
-    univId: sessionStorage.getItem('univId'),
-    sessionToken: sessionStorage.getItem('sessionToken'),
+    name: localStorage.getItem('name'),
+    email: localStorage.getItem('email'),
+    univName: localStorage.getItem('univName'),
+    uid: localStorage.getItem('uid'),
+    univId: localStorage.getItem('univId'),
+    sessionToken: localStorage.getItem('sessionToken'),
   },
   actions: {
     async createSession(context, payload: UserCredential) {
       const { user } = payload;
       if (user.email) {
-        sessionStorage.setItem('email', user.email);
+        localStorage.setItem('email', user.email);
       }
       if (user.displayName) {
-        sessionStorage.setItem('name', user.displayName);
+        localStorage.setItem('name', user.displayName);
       }
       if (user.uid) {
-        sessionStorage.setItem('uid', user.uid);
+        localStorage.setItem('uid', user.uid);
       }
       const { token, claims } = await user.getIdTokenResult();
-      sessionStorage.setItem('sessionToken', token);
-      sessionStorage.setItem('univId', claims.univId);
+      localStorage.setItem('sessionToken', token);
+      localStorage.setItem('univId', claims.univId);
       context.commit('updateStore');
     },
     async deleteSession(context) {
-      sessionStorage.clear();
+      localStorage.clear();
       context.commit('clearStore');
     },
   },
   mutations: {
     updateStore: (state) => {
-      state.name = sessionStorage.getItem('name');
-      state.email = sessionStorage.getItem('email');
-      state.univName = sessionStorage.getItem('univName');
-      state.uid = sessionStorage.getItem('uid');
-      state.univId = sessionStorage.getItem('univId');
-      state.sessionToken = sessionStorage.getItem('sessionToken');
+      state.name = localStorage.getItem('name');
+      state.email = localStorage.getItem('email');
+      state.univName = localStorage.getItem('univName');
+      state.uid = localStorage.getItem('uid');
+      state.univId = localStorage.getItem('univId');
+      state.sessionToken = localStorage.getItem('sessionToken');
     },
     clearStore: (state) => {
       state.name = null;
